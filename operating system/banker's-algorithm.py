6fea6a16bff6c94ffb01a8f970e532f89453617f
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2024-03-15 23:36:37 +0600

    Create banker's-algorithm.py

---
+++ operating system/banker's-algorithm.py
@@ -0,0 +1,56 @@
+P = 5
+R = 3
+def calculateNeed(need, maxm, allot):
+	for i in range(P):
+		for j in range(R):
+			need[i][j] = maxm[i][j] - allot[i][j]
+
+def isSafe(processes, avail, maxm, allot):
+	need = []
+	for i in range(P):
+		l = []
+		for j in range(R):
+			l.append(0)
+		need.append(l)
+
+	calculateNeed(need, maxm, allot)
+	finish = [0] * P
+	safeSeq = [0] * P
+	work = [0] * R
+	for i in range(R):
+		work[i] = avail[i]
+
+	count = 0
+	while (count < P):
+		found = False
+		for p in range(P):
+			if (finish[p] == 0):
+				for j in range(R):
+					if (need[p][j] > work[j]):
+						break
+
+				if (j == R - 1):
+					for k in range(R):
+						work[k] += allot[p][k]
+
+					safeSeq[count] = p
+					count += 1
+					finish[p] = 1
+					found = True
+
+		if (found == False):
+			print("System is not in safe state")
+			return False
+
+	print("System is in safe state.",
+			"\nSafe sequence is: "," -> ".join(f"P{p}" for p in safeSeq))
+	
+if __name__ =="__main__":
+	processes = [0, 1, 2, 3, 4]
+	avail = [3, 1, 5]
+	maxm = [[3, 2, 1], [2, 2, 1],
+			[1, 3, 4], [4, 5, 6], [2, 0, 0]]
+	allot = [[1, 1, 0], [0, 0, 3],
+			[1, 0, 3], [1, 3, 1], [0, 2, 2]]
+
+	isSafe(processes, avail, maxm, allot)
