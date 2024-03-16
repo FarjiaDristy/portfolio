90aec2877f91167620f970e23c432befdc0f3fa9
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2024-03-16 23:44:40 +0600

    Update sjf-preemptive.py

--- operating system/sjf-preemptive.py
+++ operating system/sjf-preemptive.py
@@ -1,43 +1,48 @@
-n = int(input('Enter no of processes: '))
-bt = [0] * (n + 1)
-at = [0] * (n + 1)
-abt = [0] * (n + 1)
+n = int(input('Enter the number of processes: '))
+burst_times = [0] * (n + 1)
+arrival_times = [0] * (n + 1)
+actual_burst_times = [0] * (n + 1)
 
+# Input burst time and arrival time for each process
 for i in range(n):
-    abt[i] = int(input('Enter the burst time for process {}: '.format(i + 1)))
-    at[i] = int(input('Enter the arrival time for process {}: '.format(i + 1)))
-    bt[i] = [abt[i], at[i], i]
+    actual_burst_times[i] = int(input('Enter the burst time for process {}: '.format(i + 1)))
+    arrival_times[i] = int(input('Enter the arrival time for process {}: '.format(i + 1)))
+    burst_times[i] = [actual_burst_times[i], arrival_times[i], i]
 
-bt.pop(-1)
+burst_times.pop(-1)
 
-sumbt = 0
-i = 0
-ll = []
+total_burst_time = 0
+schedule = []
 
-for i in range(0, sum(abt)):
-    l = [j for j in bt if j[1] <= i]
-    l.sort(key=lambda x: x[0])
-    bt[bt.index(l[0])][0] -= 1
-    for k in bt:
-        if k[0] == 0:
-            t = bt.pop(bt.index(k))
-            ll.append([k, i + 1])
+# Perform preemptive shortest job first scheduling
+for i in range(0, sum(actual_burst_times)):
+    arrived_processes = [j for j in burst_times if j[1] <= i]
+    if arrived_processes:
+        arrived_processes.sort(key=lambda x: x[0])
+        burst_times[burst_times.index(arrived_processes[0])][0] -= 1
+        for k in burst_times:
+            if k[0] == 0:
+                t = burst_times.pop(burst_times.index(k))
+                schedule.append([k, i + 1])
 
-ct = [0] * (n + 1)
-tat = [0] * (n + 1)
-wt = [0] * (n + 1)
+completion_times = [0] * (n + 1)
+turnaround_times = [0] * (n + 1)
+waiting_times = [0] * (n + 1)
 
-for i in ll:
-    ct[i[0][2]] = i[1]
+# Calculate completion time, turnaround time, and waiting time for each process
+for i in schedule:
+    completion_times[i[0][2]] = i[1]
 
-for i in range(len(ct)):
-    tat[i] = ct[i] - at[i]
-    wt[i] = tat[i] - abt[i]
+for i in range(len(completion_times)):
+    turnaround_times[i] = completion_times[i] - arrival_times[i]
+    waiting_times[i] = turnaround_times[i] - actual_burst_times[i]
 
-ct.pop(-1)
-wt.pop(-1)
-tat.pop(-1)
-abt.pop(-1)
-at.pop(-1)
+completion_times.pop(-1)
+waiting_times.pop(-1)
+turnaround_times.pop(-1)
+actual_burst_times.pop(-1)
+arrival_times.pop(-1)
 
-print('Average Waiting Time = {:.2f}'.format(sum(wt) / len(wt)))
+average_waiting_time = sum(waiting_times) / len(waiting_times)
+
+print('Average Waiting Time = {:.2f}'.format(average_waiting_time))
