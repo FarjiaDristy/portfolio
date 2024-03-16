20357294b8082681a4da5d3281ec7b48fc0756aa
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2024-03-17 00:30:48 +0600

    Update weekday-time-machine.cs

--- C# Projects/weekday-time-machine.cs
+++ C# Projects/weekday-time-machine.cs
@@ -6,16 +6,15 @@ namespace TimeMachineWhatWeekDay
         static void Main(string[] args)
         {
             Console.WriteLine("What year it is?");
-            int year = int.Parse(Console.ReadLine()); // or var year = Console.ReadLine();
-
+            int year = int.Parse(Console.ReadLine()); 
             Console.WriteLine("What month it is?");
             int month = int.Parse(Console.ReadLine());
 
             Console.WriteLine("What day it is?");
             int day = int.Parse(Console.ReadLine());
 
-            DateTime weekDay = new DateTime(year, month, day); // 
-            Console.WriteLine("Today is : " + weekDay.DayOfWeek); // {0} na dile , kaaj kore nai. or {0}ar , na use korle need to use + concatanation
+            DateTime weekDay = new DateTime(year, month, day);
+            Console.WriteLine("Today is : " + weekDay.DayOfWeek);
         }
     }
 }
