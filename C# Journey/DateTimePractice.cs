1aa759113e96fc68dc477527703c5b9ed84894fa
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2024-08-01 03:51:55 +0600

    DateTime Practice

--- C# Journey/DateTimePractice.cs
+++ C# Journey/DateTimePractice.cs
@@ -7,11 +7,11 @@ namespace DateTimeExTest
         {
             //First Format: 19-02-2024 01:15:55
             DateTime date = DateTime.Now;
-            string firstFormat = string.Format("First Format: {0:dd-MM-yyyy HH:mm:ss tt}", date); //small mm also works wow
+            string firstFormat = string.Format("First Format: {0:dd-MM-yyyy HH:mm:ss tt}", date);
             Console.WriteLine(firstFormat);
 
             //Second Format: Monday Of Month Frebruary 2024
-            string secondFormat = string.Format("Second Format: {0:dddd 'of Month' MMMM 'year' yyyy}" , date); // to show anything inside {0:yyyy MM dd} expression '' use single quotes to write it
+            string secondFormat = string.Format("Second Format: {0:dddd 'of Month' MMMM 'year' yyyy}" , date); 
             Console.WriteLine(secondFormat);
 
             //Third Format: Day Monday
@@ -19,11 +19,11 @@ namespace DateTimeExTest
             Console.WriteLine(thirdFormat);
 
             //Fourth Format: Year 2024
-            string fourthFormat = string.Format("Fourth Format: \n{0:'Year' yyyy}", date); // {} expression e 0: shobar agee na thakle error ashee
+            string fourthFormat = string.Format("Fourth Format: \n{0:'Year' yyyy}", date);
             Console.WriteLine(fourthFormat);
 
             //new third format Day Saturday new line month feb next line year 2024
-            fourthFormat = string.Format("Fourth Format: \n{0:'Day' dddd} \n{0:'month' MMM} \n{0:'Year' yyyy}", date); // \n new line excape sequence
+            fourthFormat = string.Format("Fourth Format: \n{0:'Day' dddd} \n{0:'month' MMM} \n{0:'Year' yyyy}", date);
             Console.WriteLine(fourthFormat);
         }
     }
