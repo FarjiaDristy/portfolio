240dfb3b3e2feb6ba15925de60d711be0b7a86b8
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2024-08-01 03:19:50 +0600

    Float Double Decimal

---
+++ C# Journey/FloatDoubleDecimal.cs
@@ -0,0 +1,27 @@
+using System;
+namespace FloatDoubleDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float f_no = 2.0555f;
+            double d_no = 2.90d;
+            decimal m_no = 34.0m;
+
+            //Formatted string is clearer than adding strings, especially when you have more than two parameters. it's for performance optimization?
+            //using placeholder or formatting (temporarily takes the place of final data) to concatenate the variables in the print line
+            Console.WriteLine("The float number is {0}" , f_no);
+            Console.WriteLine("The double number is {0}", d_no);
+            Console.WriteLine("The decimal number is {0}", m_no);
+
+            f_no = 1 / 3f;
+            d_no = 1 / 3d;
+            m_no = 1 / 3m;
+            // will show the outputs in the accurate sizes of float double and decimal. which is a precision
+            Console.WriteLine("The float number is {0}", f_no); //7 digits
+            Console.WriteLine("The double number is {0}", d_no); //16 digits
+            Console.WriteLine("The decimal number is {0}", m_no); //29 digits
+        }
+    }
+}
