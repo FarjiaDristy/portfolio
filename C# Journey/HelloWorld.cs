e9420c2574bda9fe0eb4a9781654e66c7b153875
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2024-08-01 03:11:26 +0600

    HelloWorld.cs

---
+++ C# Journey/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, Assalam-alaikum, I Hate Living!!!");
+            Console.WriteLine("it prints input in new line every time.");
+            Console.Write("it prints input in the same line.");
+            Console.Write(" doesn't go to another line.");
+        }
+    }
+}
