bdb7fabecc6ad3532e3ccd735b2b1306483426ed
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2024-10-16 03:18:28 +0600

    Find Weekday from the Calender by taking user input

---
+++ C# Problem Solving!/WeekdayCalender.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make a weekday calender by taking user input
+            Console.WriteLine("Enter the day = ");
+            var day = Console.ReadLine();
+
+            Console.WriteLine("Enter the month = ");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("Enter the year");
+            var year = Console.ReadLine();
+
+            DateTime Date = new DateTime(int.Parse(year), int.Parse(month), int.Parse(month));
+            Console.WriteLine("\n" + Date.DayOfWeek);
+
+            //OR
+            DateTime todate = new DateTime(2050, 12, 1);
+            Console.WriteLine(todate.DayOfWeek);
+        }
+    }
+}
