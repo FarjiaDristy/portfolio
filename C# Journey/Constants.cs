d0f2f39cb87827cbee59fb0a17a8acf5ecdfde2c
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2024-10-20 22:18:32 +0600

    Constants keyword/datatype

--- C# Journey/Constants.cs
+++ C# Journey/Constants.cs
@@ -8,7 +8,6 @@ class Program
             //use const keyword to declare var with fixed values
             const double gravityForce = 9.8;
             const double PI = 3.14159265359;
-            // gravityForce = 1; //it's wrong so to avoid it use const datatype to fix its value and cannot assign any new value
             Console.WriteLine("Gravity Force is: {0} and PI value is: {1}.", gravityForce , PI);
             Console.WriteLine("Gravity Forse is: " + gravityForce + " and PI value is: " + PI + "." );
             
