74bf7537b6b90c72f5fec8747ef0200b8913a8b5
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2024-11-20 12:51:54 +0600

    Delete C# Problem Solving!/StringManipulationEx.cs

--- C# Problem Solving!/StringManipulationEx.cs
+++
@@ -1,6 +0,0 @@
-//take two user inputs, remove unnecessary white spaces, and print using string manipulation.
-string movieTitle = Console.ReadLine().Trim();
-string movieSeason = Console.ReadLine().Trim();
-string newMovie = "";
-newMovie = movieTitle.Insert(movieTitle.Trim().Length, " - " + movieSeason.Trim());
-Console.WriteLine(newMovie);
