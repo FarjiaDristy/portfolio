89b3d1c8a8b9b315e7eb39c7e315ef0380bf2c09
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2024-11-20 12:49:25 +0600

    string Manipulation Exercise

--- C# Problem Solving!/stringManipulationExercise.cs
+++ C# Problem Solving!/stringManipulationExercise.cs
@@ -6,10 +6,11 @@ namespace StringManipulationExercise
     {
         static void Main(string[] args)
         {
+            //take two user inputs, remove unnecessary white spaces, and print using string manipulation.
             string movieTitle = Console.ReadLine().Trim();
             string movieSeason = Console.ReadLine().Trim();
             string newMovie = "";
-            newMovie = movieTitle.Insert(movieTitle.Trim().Length, " - " + movieSeason.Trim());
+            newMovie = movieTitle.Insert(movieTitle.Length, " - " + movieSeason + ".");
             Console.WriteLine(newMovie);
         }
     }
