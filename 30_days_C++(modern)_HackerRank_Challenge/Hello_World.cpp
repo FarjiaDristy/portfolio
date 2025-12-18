8f7b264cb23849b77c448f630a892f25cd181bba
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-19 01:02:36 +0600

    Create Hello_World.cpp
    To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.

---
+++ 30_days_C++(modern)_HackerRank_Challenge/Hello_World.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    std::string input_string; 
+    
+    std::getline(cin, input_string); 
+
+    std::cout << "Hello, World." << std::endl;
+
+    std::cout << input_string << std::endl;
+
+    return 0;
+}
