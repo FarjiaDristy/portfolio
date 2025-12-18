919d6f400be9e66dffed07ed1b43a2f9509b7ab2
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-19 00:06:12 +0600

    Create Hello_world.cs
    To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.

---
+++ 30_days_C#_HackerRank_Challenge/Hello_world.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    static void Main(String[] args) {
+
+        String inputString; 
+        
+        inputString = Console.ReadLine(); 
+
+        Console.WriteLine("Hello, World.");
+        Console.WriteLine(inputString);
+    }
+}
