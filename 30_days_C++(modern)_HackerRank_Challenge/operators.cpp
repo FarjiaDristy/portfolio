c9f8ca5d89d8736946d763625d5b647fb947b391
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-20 02:10:27 +0600

    Create operators.cpp
    Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.

---
+++ 30_days_C++(modern)_HackerRank_Challenge/operators.cpp
@@ -0,0 +1,62 @@
+# c++11, c+=14, c++20
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+void solve(double meal_cost, int tip_percent, int tax_percent) {
+
+}
+
+int main()
+{
+    string meal_cost_temp;
+    getline(cin, meal_cost_temp);
+
+    double meal_cost = stod(ltrim(rtrim(meal_cost_temp)));
+
+    string tip_percent_temp;
+    getline(cin, tip_percent_temp);
+
+    int tip_percent = stoi(ltrim(rtrim(tip_percent_temp)));
+
+    string tax_percent_temp;
+    getline(cin, tax_percent_temp);
+
+    int tax_percent = stoi(ltrim(rtrim(tax_percent_temp)));
+
+    solve(meal_cost, tip_percent, tax_percent);
+    
+    double tip = meal_cost * tip_percent / 100;
+    double tax = meal_cost * tax_percent / 100;
+    double total = meal_cost + tip + tax;
+    
+    cout << round(total) << endl;
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
