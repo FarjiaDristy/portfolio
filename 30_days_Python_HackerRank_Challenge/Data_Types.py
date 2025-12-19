330d578b4e01807f31c762171a47bf57f4480bc6
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-19 12:19:51 +0600

    Create Data_Types.py
    Complete the code in the editor below. The variables i, d, and s are already declared and initialized for you. You must:
    
    Declare 3 variables: one of type int, one of type double, and one of type String.
    Read 3 lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your 3 variables.
    Use the + operator to perform the following operations:
    Print the sum of i plus your int variable on a new line.
    Print the sum of d plus your double variable to a scale of one decimal place on a new line.
    Concatenate s with the string you read as input and print the result on a new line.

---
+++ 30_days_Python_HackerRank_Challenge/Data_Types.py
@@ -0,0 +1,29 @@
+#pytho-2
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+i2 = int(raw_input())
+d2 = float(raw_input())
+s2 = raw_input()
+
+print i + i2
+print "%.1f" % (d + d2)
+print s + s2
+
+#////
+
+#python-3
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+i2 = int(input())
+d2 = float(input())
+s2 = input()
+
+print(i + i2)
+print("%.1f" % (d + d2))
+#print(f"{d + d2:.1f}")
+print(s + s2)
+
