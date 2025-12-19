3bb6fd784b5d249d990030da7ded7cd4e14451de
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2025-12-20 02:39:46 +0600

    Create Conditional#1.py
    Given an integer, , perform the following conditional actions:
    
    If n is odd, print Weird
    If n is even and in the inclusive range of 2 to 5, print Not Weird
    If n is even and in the inclusive range of 6 to 20, print Weird
    If n is even and greater than 20, print Not Weird
    Complete the stub code provided in your editor to print whether or not n is weird.

---
+++ 30_days_Python_HackerRank_Challenge/Conditional#1.py
@@ -0,0 +1,40 @@
+#python-2
+#!/bin/python
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    N = int(raw_input().strip())
+    
+if N % 2 != 0:
+    print "Weird"
+elif 2 <= N <= 5:
+    print "Not Weird"
+elif 6 <= N <= 20:
+    print "Weird"
+else:
+    print "Not Weird"
+
+#python-3
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    
+if N % 2 != 0:
+    print("Weird")
+elif 2 <= N <= 5:
+    print("Not Weird")
+elif 6 <= N <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
+
