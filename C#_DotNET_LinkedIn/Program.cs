69aec3eea5ccfdfaa503e7d6385c7063cf74330f
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2026-03-02 23:37:25 +0600

    Refactor Program.cs to enhance output and structure

--- C#_DotNET_LinkedIn/Program.cs
+++ C#_DotNET_LinkedIn/Program.cs
@@ -1,29 +1,29 @@
 using System;
+
 class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("DotNET LinkedIn Program started\n");
+
         StringInterpolation.Run();
+
+        Console.WriteLine("\n----------------\n");
+
         StringFormatting.Run();
 
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\n----------------\n");
 
-        int a = 100;
-        float b = 250.50f;
-        string c = "CSharp";
+        StringManipulation.Run();
 
-        // default
-        Console.WriteLine("The value of a={0}, the value of b={1} and the value of c={2}", a, b, c);
+        Console.WriteLine("\n----------------\n");
 
-        // string interpolation
-        Console.WriteLine($"The value of a={a}, the value of b={b} and the value of c={c}");
+        StringSearching.Run();
 
-        // interpolated string
-        Console.WriteLine($"The value of (a + b)/b={(a + b) / b}");
-        Console.WriteLine($"The value of {c} in upper-case={c.ToUpper()}");
+        Console.WriteLine("\n----------------\n");
 
-        // objects embedded in string interpolation
-        DateTime now = DateTime.Now;
-        Console.WriteLine($"The current date and time is {now}");
+        MathExample.Run();
+
+        Console.WriteLine("\nProgram finished");
     }
-}   
+}
