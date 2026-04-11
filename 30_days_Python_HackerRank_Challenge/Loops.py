8b4f09f599cfef1be3375d2740e70c7ad10dfe44
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2026-04-11 22:17:54 +0600

    Fix indentation and formatting in Loops.py

--- 30_days_Python_HackerRank_Challenge/Loops.py
+++ 30_days_Python_HackerRank_Challenge/Loops.py
@@ -1,6 +1,5 @@
-#python-2
+#Python-2
 #!/bin/python
-
 import math
 import os
 import random
@@ -9,8 +8,9 @@ import sys
 
 if __name__ == '__main__':
     n = int(raw_input().strip())
-    for i in range(1, 11):
-        print "%d x %d = %d" % (n, i, n * i)
+
+for i in range(1, 11):
+    print "%d x %d = %d" % (n, i, n * i)
 
 #python-3
 #!/bin/python3
@@ -21,9 +21,8 @@ import random
 import re
 import sys
 
-
-
 if __name__ == '__main__':
     n = int(input().strip())
-    for i in range (1, 11):
-        print(f"{n} x {i} = {n *i}")
+
+for i in range(1, 11):
+    print(f"{n} x {i} = {n * i}")
