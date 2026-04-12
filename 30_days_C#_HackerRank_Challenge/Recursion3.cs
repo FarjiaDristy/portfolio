a74ce492eba604f2c1615fbe423a5a7decd5df6d
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2026-04-12 14:38:22 +0600

    Remove unnecessary whitespace in Recursion3.cs

---
+++ 30_days_C#_HackerRank_Challenge/Recursion3.cs
@@ -0,0 +1,43 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    public static int factorial(int n)
+    {
+    if (n <= 1)
+        return 1;
+
+    return n * factorial(n - 1);
+}
+
+    }
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.factorial(n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
