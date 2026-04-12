3489fef7abe844ac703dad558fe97762264a323c
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2026-04-12 17:45:18 +0600

    Implement inheritance with Person and Student classes

---
+++ 30_days_C#_HackerRank_Challenge/Inheritance.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+
+class Person{
+	protected string firstName;
+	protected string lastName;
+	protected int id;
+	
+	public Person(){}
+	public Person(string firstName, string lastName, int identification){
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.id = identification;
+	}
+	public void printPerson(){
+		Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id); 
+	}
+}
+
+
+class Student : Person{
+    private int[] scores;  
+  
+    public Student(string firstName, string lastName, int idNumber, int[] scores)
+        : base(firstName, lastName, idNumber)
+    {
+        this.scores = scores;
+    }
+
+    public char Calculate() {
+        int sum = 0;
+        foreach (int s in scores)
+            sum += s;
+
+        int avg = sum / scores.Length;
+
+        if (avg >= 90) return 'O';
+        if (avg >= 80) return 'E';
+        if (avg >= 70) return 'A';
+        if (avg >= 55) return 'P';
+        if (avg >= 40) return 'D';
+        return 'T';
+    }
+}
+
+class Solution {
+	static void Main() {
+		string[] inputs = Console.ReadLine().Split();
+		string firstName = inputs[0];
+	  	string lastName = inputs[1];
+		int id = Convert.ToInt32(inputs[2]);
+		int numScores = Convert.ToInt32(Console.ReadLine());
+		inputs = Console.ReadLine().Split();
+	  	int[] scores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			scores[i]= Convert.ToInt32(inputs[i]);
+		} 
+	  	
+		Student s = new Student(firstName, lastName, id, scores);
+		s.printPerson();
+		Console.WriteLine("Grade: " + s.Calculate() + "\n");
+	}
+}
