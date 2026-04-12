137970449e16555444998ce979b92046c8c01e38
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2026-04-12 16:22:14 +0600

    Add BinaryNumbers.cpp to count consecutive ones

---
+++ 30_days_C++(modern)_HackerRank_Challenge/BinaryNumbers.cpp
@@ -0,0 +1,58 @@
+#c++11,14,20
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+    
+    cin >> n;
+
+    int maxCount = 0, current = 0;
+
+    while (n > 0) {
+        if (n & 1) {
+            current++;
+            if (current > maxCount)
+                maxCount = current;
+        } else {
+            current = 0;
+        }
+        n >>= 1;
+    }
+
+    cout << maxCount;
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
