b2269a61c5ceb0d6571b58ba807d5a7c76a0d68b
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2026-04-13 15:51:46 +0600

    Add palindrome check using stack and queue
    Implement a solution to check if a string is a palindrome using stack and queue.

---
+++ 30_days_C++(modern)_HackerRank_Challenge/Queues&Stacks.cpp
@@ -0,0 +1,70 @@
+#CPP 11 14 20
+#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+    public:
+    stack<char> st;
+    queue<char> qu;
+
+    void pushCharacter(char ch) {
+        st.push(ch);
+    }
+
+    void enqueueCharacter(char ch) {
+        qu.push(ch);
+    }
+
+    char popCharacter() {
+        char ch = st.top();
+        st.pop();
+        return ch;
+    }
+
+    char dequeueCharacter() {
+        char ch = qu.front();
+        qu.pop();
+        return ch;
+    }
+};
+
+
+int main() {
+    // read the string s.
+    string s;
+    getline(cin, s);
+    
+  	// create the Solution class object p.
+    Solution obj;
+    
+    // push/enqueue all the characters of string s to stack.
+    for (int i = 0; i < s.length(); i++) {
+        obj.pushCharacter(s[i]);
+        obj.enqueueCharacter(s[i]);
+    }
+    
+    bool isPalindrome = true;
+    
+    // pop the top character from stack.
+    // dequeue the first character from queue.
+    // compare both the characters.
+    for (int i = 0; i < s.length() / 2; i++) {
+        if (obj.popCharacter() != obj.dequeueCharacter()) {
+            isPalindrome = false;
+            
+            break;
+        }
+    }
+    
+    // finally print whether string s is palindrome or not.
+    if (isPalindrome) {
+        cout << "The word, " << s << ", is a palindrome.";
+    } else {
+        cout << "The word, " << s << ", is not a palindrome.";
+    }
+    
+    return 0;
+}
