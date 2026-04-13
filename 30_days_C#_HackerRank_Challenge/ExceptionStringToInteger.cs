7c93e4fdf565b56f58b883d8d227fd13ebff8cc2
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2026-04-13 10:43:13 +0600

    Add ExceptionStringToInteger.cs for string parsing

---
+++ 30_days_C#_HackerRank_Challenge/ExceptionStringToInteger.cs
@@ -0,0 +1,31 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string S = Console.ReadLine();
+        try
+        {
+            int num = int.Parse(S);
+            Console.WriteLine(num);
+        }
+        catch
+        {
+            Console.WriteLine("Bad String");
+        }
+    }
+    
+}
