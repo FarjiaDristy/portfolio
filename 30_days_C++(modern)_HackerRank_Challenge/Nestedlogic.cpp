865f583bb6bb418e6c2f8ee84f70c2a9129db8e6
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2026-04-28 22:47:29 +0600

    Add Nestedlogic.cpp for calculating fine based on dates

---
+++ 30_days_C++(modern)_HackerRank_Challenge/Nestedlogic.cpp
@@ -0,0 +1,26 @@
+#cpp 11 14 20
+#include <iostream>
+using namespace std;
+
+int main() {
+    int d1, m1, y1;
+    int d2, m2, y2;
+    
+    cin >> d1 >> m1 >> y1;
+    cin >> d2 >> m2 >> y2;
+    
+    int fine = 0;
+    
+    if (y1 > y2) {
+        fine = 10000;
+    } else if (y1 == y2) {
+        if (m1 > m2) {
+            fine = 500 * (m1 - m2);
+        } else if (m1 == m2 && d1 > d2) {
+            fine = 15 * (d1 - d2);
+        }
+    }
+    
+    cout << fine << endl;
+    return 0;
+}
