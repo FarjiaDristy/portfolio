f64cc121329766976a405cdbdefc32db795bc71e
FarjiaDristy <160651221+FarjiaDristy@users.noreply.github.com>
2026-05-13 23:36:06 +0600

    Add factorial function and input handling

---
+++ 10_days_JavaScript_HackerRank_Challenge/Functions.js
@@ -0,0 +1,40 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function factorial(n) {
+    let result = 1;
+    
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    
+    return result;
+}
+
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
